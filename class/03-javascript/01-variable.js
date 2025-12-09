621a5b81a40166d5e0078e104a83e87b9e300093
msho <7fingersstudio@gmail.com>
2025-12-09 21:45:55 +0900

    javascript 연습

---
+++ class/03-javascript/01-variable.js
@@ -0,0 +1,5 @@
+console.log("콘솔로그를 찍어 봅니다.");
+
+const name = "홍길동";
+
+console.log(name);
---
+++ class/03-javascript/연습-01-array.js
@@ -0,0 +1,26 @@
+console.log("콘솔로그를 찍어 봅니다.");
+let classmates = ["철수", "영희", "훈이"];
+
+console.log(classmates[1]);
+
+console.log(classmates.includes("맹구"));
+
+console.log(classmates.length);
+
+classmates.push("맹구");
+
+console.log(classmates);
+
+console.log(classmates.includes("맹구"));
+
+console.log(classmates.length);
+
+classmates.pop();
+
+console.log(classmates);
+
+console.log(classmates.length);
+
+let dream = ["축구선수", "과학자", "파일럿  "];
+
+console.log(classmates.concat(dream));
