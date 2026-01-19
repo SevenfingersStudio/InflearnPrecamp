a959f91f9fabf48593dc88fce9532e5a1b0bd332
msho <7fingersstudio@gmail.com>
2026-01-19 23:28:50 +0900

    끝말잇기 미니홈피 만들기전

---
+++ class/03-javascript/05-function.js
@@ -0,0 +1,19 @@
+const auth = () => {
+  const token = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+  document.getElementById("target").innerText = token;
+  document.getElementById("target").style.color = "#" + token;
+
+  let time = 5;
+
+  setInterval(() => {
+    if (time >= 0) {
+      let min = Math.floor(time / 60);
+      let sec = String(time % 60).padStart(2, "0");
+
+      document.getElementById("timer").innerText = min + ":" + sec;
+      time -= 1;
+    } else {
+      document.getElementById("finish").disabled = true;
+    }
+  }, 1000);
+};
